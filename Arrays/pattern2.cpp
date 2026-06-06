f621326f0a198b69bfa32979b73d6c5296ebe91e
Shivani Singh <shivanikumari200631@gmail.com>
2026-06-06 23:40:24 +0530

    add 2nd pattern

---
+++ Arrays/pattern2.cpp
@@ -0,0 +1,13 @@
+// pattern2.cpp
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
